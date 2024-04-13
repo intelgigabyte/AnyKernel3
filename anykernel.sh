fa7c3ac33644bfbc701852eaa813e223ae43b055
Gian Paolo <gianpaoloestacio5@gmail.com>
2024-04-13 11:13:05 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=回 Kernel For Realme C25/C25s/Narzo 50A
+kernel.string=Mafia Kernel For Realme C25/C25s/Narzo 50A
 do.devicecheck=0
 do.modules=0
 do.systemless=1
