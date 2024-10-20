af6634a0f600465003f211b352b1c0558b04e212
Gian Paolo <gianpaoloestacio5@gmail.com>
2024-10-20 22:42:43 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Mafia Kernel by CartelPH
+kernel.string=Cartel Kernel by eun0115
 do.devicecheck=1
 do.modules=0
 do.systemless=1
